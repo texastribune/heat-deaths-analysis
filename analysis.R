f187b212a15313d9e78163c73afa4862711136f5
Yuriko-Schumacher <yuriko.schumacher@gmail.com>
2024-08-14 02:48:04 -0500

    edits on the code

--- analysis.R
+++ analysis.R
@@ -10,7 +10,7 @@ df_processed <- read.csv("data/processed.csv") %>%
   # only using april to september in this setting
   filter(month >= 4 & month <= 9) 
 
-# County name
+# County names
 counties <- df_processed$county %>% 
   unique()
 
@@ -58,43 +58,37 @@ for(i in 1:length(counties)) {
   new_df_county$lo <- ginv(prs[[1]] - 1.96 * prs[[2]])
   new_df_county$up <- ginv(prs[[1]] + 1.96 * prs[[2]])
   
-  # go back to original distribution of heat-event days
+  # original distribution of heat-event days
   new_df_county$is_heat_event <- df_county$is_heat_event
   
-  # subset of heat-event days
+  # subset of heat-event days and non heat-event days
   new_df_county_heat_days <- subset(
     new_df_county,
     is_heat_event == T
   )
-  
-  # subset of non-heat-event days
   new_df_county_non_heat_days <- subset(
     new_df_county,
     is_heat_event == F
   )
   
-  # Tally up the number of actual deaths on heat-event event days
+  # actual and predicted deaths on heat-event event days
   deaths.heatdays <- sum(new_df_county_heat_days$deaths)
-  
-  # Tally up the predicted number of deaths on heat-event event days
   deaths.predicted <- sum(new_df_county_heat_days$pred)
   
-  # Calculate relative risk
+  # relative risk
   rr <- deaths.heatdays / deaths.predicted
-  # And its margin of error
   rr.lo <- exp(log(rr - 1.96 * sqrt(1 / deaths.heatdays + 1 / deaths.predicted)))
   rr.up <- exp(log(rr + 1.96 * sqrt(1 / deaths.heatdays + 1 / deaths.predicted)))
   
-  # Excess deaths is difference between observed and expected
+  # excess deaths
   excess <- sum(new_df_county_heat_days$deaths - new_df_county_heat_days$pred)
-  # And its margin of error
   excess_up <- sum(new_df_county_heat_days$deaths - new_df_county_heat_days$lo)
   excess_lo <- sum(new_df_county_heat_days$deaths - new_df_county_heat_days$up)
   
-  # Get the average population of the county
+  # average population
   avg_population <- mean(df_county$population)
   
-  # Output row
+  # output row
   county_output <- data.frame(
     county = c,
     avg_population,
